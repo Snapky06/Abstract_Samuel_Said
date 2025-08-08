025a1948d92702aa52c1689aa9cf7e724f23b8d5
Snapky06 <saidnapky@gmail.com>
2025-08-08 10:50:38 -0600

    probando

---
+++ src/abstract_samuel_said/ModeloSudoku.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package abstract_samuel_said;
+
+/**
+ *
+ * @author saidn
+ */
+public class ModeloSudoku {
+    
+}
