221a3d06c6fd9f0d70b281a4b96c65fcffc59c5a
Snapky06 <saidnapky@gmail.com>
2025-08-08 08:26:42 -0600

    probando

---
+++ src/abstract_samuel_said/Abstract_Samuel_Said.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package abstract_samuel_said;
+
+/**
+ *
+ * @author saidn
+ */
+public class Abstract_Samuel_Said {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    //amsdoamdadaosdmasdo
+}
